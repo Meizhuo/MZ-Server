64fc96cdfb6cc4886bde1fee44288b1d56a42ecc
Jayin Ton <tonjayin@gmail.com>
2014-09-26 20:04:13 +0800

    draft

--- Docs/create_tables.sql
+++ Docs/create_tables.sql
@@ -18,97 +18,148 @@ create table `mz_subsidy_standary` (
 );
 
 -- 用户表
---    `birthday` int(11) DEFAULT '0' COMMENT '生日',
 DROP TABLE IF EXISTS `mz_user`;
-create table `mz_user`(
-    `uid` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-    `nickname`  varchar(32) NOT NULL COMMENT '用户昵称',
-    `sex` varchar(8) NOT NULL DEFAULT '男' COMMENT '性别',
-    `work_place` varchar(256) DEFAULT '' COMMENT '工作单位',
-    `phone` varchar(32) COMMENT '手机号码',
-    `email` varchar(32) COMMENT '电子邮箱',
-    `reg_time` int(11) NOT NULL COMMENT '注册时间',
-    `psw` varchar(128) NOT NULL COMMENT '密码(8-16位)',
-    `level` int(3) NOT NULL COMMENT '权限等级(2个人用户 4用人单位 8培训机构16管理员 32超级管理员 )',
-    `status` int(2) NOT NULL  COMMENT '审核状态(-1审核不通过 0 未审核1审核通过)',
-    PRIMARY KEY (`uid`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表' AUTO_INCREMENT=1 ;
+CREATE TABLE `mz_user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id (主键)',
+  `nickname` varchar(128) NOT NULL COMMENT ' 用户昵称',
+  `phone` varchar(32) NOT NULL COMMENT '手机号码(11位)',
+  `email` varchar(32) DEFAULT NULL COMMENT '邮箱',
+  `psw` varchar(64) NOT NULL COMMENT '密码md5',
+  `level` int(1) NOT NULL DEFAULT '1' COMMENT '权限等级(1个人用户 2 用人单位 4 培训机构8管理员16超级管理员 )',
+  `status` int(1) NOT NULL DEFAULT '0' COMMENT '审核状态(-1审核不通过 0 未审核1审核通过)',
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `nickname` (`nickname`,`phone`,`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表' AUTO_INCREMENT=1 ;
+
+
+-- 个人用户表
+DROP TABLE IF EXISTS `mz_user_person`;
+CREATE TABLE `mz_user_person` (
+  `uid` int(11) NOT NULL COMMENT 'uid用户id',
+  `sex` varchar(2) DEFAULT NULL COMMENT '性别(男或女)',
+  `work_place` varchar(128) DEFAULT NULL COMMENT '工作地点',
+  `reg_time` int(11) NOT NULL COMMENT '注册时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='个人用户表';
+
+
+-- 用人单位
+DROP TABLE IF EXISTS `mz_user_employer`;
+CREATE TABLE  `mz_user_employer` (
+  `uid` int(11) NOT NULL COMMENT '用户id',
+  `phone` varchar(32) DEFAULT NULL COMMENT '联系电话',
+  `address` varchar(128) DEFAULT NULL COMMENT '用人单位地址',
+  `reg_time` int(11) NOT NULL COMMENT '注册时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用人单位';
+
+-- 管理员表
+DROP TABLE IF EXISTS `mz_user_admin`;
+CREATE TABLE `mz_user_admin` (
+  `uid` int(11) NOT NULL COMMENT '用户id',
+  `reg_time` int(11) NOT NULL COMMENT '注册时间',
+  `per_categorys_post` text  COMMENT '有权限起草/编辑的栏目 ',
+  `per_categorys_check` text COMMENT '有权限管理的群组',
+  `per_institution_check` text  COMMENT '有权限审核培训机构'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员表';
+
+-- 培训机构用户表
+DROP TABLE IF EXISTS `mz_user_institution`;
+CREATE TABLE `mz_user_institution` (
+  `uid` int(11) NOT NULL COMMENT '机构用户id',
+  `reg_time` int(11) NOT NULL COMMENT '注册时间',
+  `name` varchar(128) DEFAULT NULL COMMENT '机构名称',
+  `address` varchar(256) DEFAULT NULL COMMENT '机构地址',
+  `manager` varchar(128) DEFAULT NULL COMMENT '机构负责人 ',
+  `type` varchar(128) DEFAULT NULL COMMENT '办学类型',
+  `approval_number` varchar(256) DEFAULT NULL COMMENT '批准文号',
+  `validity_date` int(11) DEFAULT NULL COMMENT '有效期 ',
+  `training_scope` text COMMENT '培训范围',
+  `description` text COMMENT '描述',
+  `teacher_resource` text COMMENT '师资力量'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='培训机构用户表';
+
+
+-- 培训课程表
+DROP TABLE IF EXISTS `mz_course`;
+CREATE TABLE  `mz_course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '课程id',
+  `institution_id` int(11) NOT NULL COMMENT '所属的培训机构',
+  `subsidy_id` int(11) DEFAULT NULL COMMENT '对应的补贴项目',
+  `name` varchar(64) DEFAULT NULL COMMENT '课程名称',
+  `start_time` int(11) DEFAULT NULL COMMENT '开课时间',
+  `address` varchar(256) DEFAULT NULL COMMENT '开课地址',
+  `teacher` varchar(32) DEFAULT NULL COMMENT '授课老师',
+  `introduction` text COMMENT '课程介绍',
+  `cost` int(11) DEFAULT NULL COMMENT '课程费用',
+  `end_time` int(11) DEFAULT NULL COMMENT '开课结束时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='培训课程' AUTO_INCREMENT=1 ;
+
+
+
 
 -- 文档(新闻、文章)
 DROP TABLE IF EXISTS `mz_document`;
-create table `mz_document`(
-    `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '文档id',
-    `uid` int(11) NOT NULL COMMENT '作者ID',
-    `title` varchar(64) COMMENT '标题',
-    `category_id` int(3) NOT NULL COMMENT '所属分类',
-    `display` int(1) NOT NULL  COMMENT '可见性(-1未审核 0 不可见 1可见)',
-    `views` int(11) NOT NULL DEFAULT '0' COMMENT '浏览量',
-    `commoents` int(11) NOT NULL DEFAULT '0' COMMENT '评论数',
-    `create_time`  int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
-    `update_time`  int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
-    `from` varchar(256) COMMENT '新闻来源',
-    `level` int(3) NOT NULL NOT NULL DEFAULT '0' COMMENT '文章等级(0 全部可见 2 只有个人用户可见 4 只有用人单位可见  8只有培训机构可见16 只有管理员可见 32只有超级管理员可见)',
-     PRIMARY KEY (`id`) 
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文档' AUTO_INCREMENT=1 ;
+CREATE TABLE  `mz_document` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '文档id ',
+  `uid` int(11) NOT NULL COMMENT '作者id',
+  `title` varchar(256) NOT NULL COMMENT '标题 ',
+  `category_id` int(11) NOT NULL COMMENT '所属分类(栏目)',
+  `display` int(2) NOT NULL DEFAULT '1' COMMENT '可见性( 0 不可见1可见,默认1可见)',
+  `status` int(2) NOT NULL DEFAULT '0' COMMENT '审核状态(-1审核不通过 0未审核 1审核通过)',
+  `views` int(11) NOT NULL DEFAULT '0' COMMENT '浏览量(默认为0)',
+  `create_time` int(11) NOT NULL COMMENT '创建时间',
+  `update_time` int(11) NOT NULL COMMENT '最后更新时间',
+  `from` varchar(256) DEFAULT NULL COMMENT '新闻来源(可空',
+  `level` int(2) NOT NULL COMMENT '文章等级(1 只有个人用户可见 2 只有用人单位可见  4只有培训机构可见8 只有管理员可见 16只有超级管理员可见 31全部可见,默认31',
+  `description` text COMMENT '描述(可空',
+  `order_num` int(1) NOT NULL DEFAULT '1' COMMENT '序号(优先级别,默认为1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文档' AUTO_INCREMENT=1 ;
 
--- 新闻详情
+-- 文档详情
 DROP TABLE IF EXISTS `mz_document_detail`;
-create table `mz_document_detail`(
-    `id` int(11) NOT NULL DEFAULT '0' COMMENT '文档ID',
-    `content` text NOT NULL DEFAULT '' COMMENT '内容',
-    `files` text COMMENT '附件下载地址'
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文档类型' ;
+CREATE TABLE  `mz_document_detail` (
+  `id` int(11) NOT NULL COMMENT '新闻id',
+  `content` text NOT NULL COMMENT '新闻内容'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文档详情';
 
--- 文档类型
+
+-- 文档附件
+DROP TABLE IF EXISTS `mz_document_file`;
+CREATE TABLE `mz_document_file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '附件id',
+  `doc_id` int(11) NOT NULL COMMENT '对应的文档id',
+  `name` varchar(64) NOT NULL COMMENT '原始文件名',
+  `savepath` varchar(512) NOT NULL COMMENT '文件保存路径 ',
+  `ext` int(32) NOT NULL COMMENT '文件后缀',
+  `mime` int(32) DEFAULT NULL COMMENT '文件mime类型',
+  `size` int(11) DEFAULT NULL COMMENT '文件大小',
+  `md5` varchar(64) DEFAULT NULL COMMENT '文件md5',
+  `create_time` int(11) NOT NULL COMMENT '上传时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文档附件' AUTO_INCREMENT=1 ;
+
+
+-- 文档种类/栏目
 DROP TABLE IF EXISTS `mz_document_type`;
-create table `mz_document_type` (
-    `category_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '分类id',
-    `name` varchar(128) NOT NULL DEFAULT '' COMMENT '种类名称' ,
-    `description` text  COMMENT '描述',
-    PRIMARY KEY (`category_id`) 
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文档类型' AUTO_INCREMENT=1 ;
+CREATE TABLE `mz_document_type` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '分类id',
+  `name` varchar(32) NOT NULL COMMENT '种类名称',
+  `description` text COMMENT '描述',
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文档种类/栏目' AUTO_INCREMENT=1 ;
 
--- 应用信息 
-DROP TABLE IF EXISTS `mz_appinfo`;
-create table `mz_appinfo` (
-    `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '记录id',
-    `version_name` varchar(32) NOT NULL DEFAULT '1.0.0' COMMENT '版本代号',
-    `version_code`  int(10) NOT NULL DEFAULT '1'  COMMENT '版本号',
-    `description` text  COMMENT '版本描述',
-     PRIMARY KEY (`id`) 
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='应用信息' AUTO_INCREMENT=1 ;
-
-
--- 培训机构 `mz_institution`
-
-CREATE TABLE IF NOT EXISTS `mz_institution` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '机构id ',
-  `name` varchar(128) NOT NULL COMMENT '机构名称',
-  `address` varchar(256) NOT NULL COMMENT '机构地址',
-  `manager` varchar(32) NOT NULL COMMENT '机构负责人',
-  `type` varchar(32) NOT NULL COMMENT '办学类型',
-  `approval_number` varchar(256) NOT NULL COMMENT '批准文号',
-  `validity_date` int(11) NOT NULL COMMENT '有效期',
-  `training_scope` varchar(512) NOT NULL COMMENT '培训范围',
-  `description` text NOT NULL COMMENT '描述',
-  `status` int(1) NOT NULL DEFAULT '0' COMMENT '审核状态(-1审核失败 0未审核 1已审核)',
+-- 消息
+DROP TABLE IF EXISTS `mz_message`;
+CREATE TABLE `mz_message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '消息id',
+  `uid` int(11) NOT NULL COMMENT '发送人id',
+  `title` varchar(64) NOT NULL COMMENT '标题',
+  `content` text NOT NULL COMMENT '内容',
+  `create_time` int(11) NOT NULL COMMENT '创建日期',
+  `status` int(1) NOT NULL DEFAULT '0' COMMENT '发送状态(-1发送失败 0等待发送 1发送成功) ',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='培训机构' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='消息' AUTO_INCREMENT=1 ;
 
 
---
--- 培训课程 `mz_course`
---
 
-CREATE TABLE IF NOT EXISTS `mz_course` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '课程id',
-  `name` varchar(64) NOT NULL COMMENT '课程名称',
-  `start_time` int(11) NOT NULL COMMENT '开课时间',
-  `address` varchar(256) NOT NULL COMMENT '开课地址',
-  `teacher` varchar(4096) NOT NULL COMMENT '授课老师',
-  `introduction` varchar(4096) NOT NULL COMMENT '课程介绍',
-  `cost` int(11) NOT NULL COMMENT '课程费用',
-  `end_time` int(11) NOT NULL COMMENT '开课结束时间',
-  `institution` int(11) NOT NULL COMMENT '所属的培训机构id',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='培训课程' AUTO_INCREMENT=1 ;
