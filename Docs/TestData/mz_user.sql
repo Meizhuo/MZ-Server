d0a53d4442d127b5af2db8c7b934437b941133d2
Jayin Ton <tonjayin@gmail.com>
2014-10-06 19:12:58 +0800

    test data

---
+++ Docs/TestData/mz_user.sql
@@ -0,0 +1,36 @@
+
+--
+-- 转存表中的数据 `mz_user`
+--
+
+INSERT INTO `mz_user` (`uid`, `nickname`, `phone`, `email`, `psw`, `reg_time`, `level`, `status`) VALUES
+
+INSERT INTO `mz_user` (`uid`, `nickname`, `phone`, `email`, `psw`, `reg_time`, `level`, `status`) VALUES
+(94, '竭诚培训公司1', NULL, 'user1@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586535, 4, 1),
+(95, '竭诚培训公司2', NULL, 'user2@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586535, 4, 1),
+(96, '竭诚培训公司3', NULL, 'user3@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586535, 4, 1),
+(97, '竭诚培训公司4', NULL, 'user4@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586535, 4, 1),
+(98, '竭诚培训公司5', NULL, 'user5@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586535, 4, 1),
+(99, '竭诚培训公司6', NULL, 'user6@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586535, 4, 1),
+(100, '竭诚培训公司7', NULL, 'user7@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586535, 4, 1),
+(101, '竭诚培训公司8', NULL, 'user8@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586535, 4, 1),
+(102, '竭诚培训公司9', NULL, 'user9@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586535, 4, 1),
+(103, '竭诚培训公司10', NULL, 'user10@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586536, 4, 1),
+(104, '竭诚培训公司11', NULL, 'user11@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586536, 4, 1),
+(105, '竭诚培训公司12', NULL, 'user12@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586536, 4, 1),
+(106, '竭诚培训公司13', NULL, 'user13@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586536, 4, 1),
+(107, '竭诚培训公司14', NULL, 'user14@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586536, 4, 1),
+(108, '竭诚培训公司15', NULL, 'user15@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586536, 4, 1),
+(109, '竭诚培训公司16', NULL, 'user16@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586536, 4, 1),
+(110, '竭诚培训公司17', NULL, 'user17@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586536, 4, 1),
+(111, '竭诚培训公司18', NULL, 'user18@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586536, 4, 1),
+(112, '竭诚培训公司19', NULL, 'user19@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586536, 4, 1),
+(113, '竭诚培训公司20', NULL, 'user20@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586537, 4, 1),
+(114, '竭诚培训公司21', NULL, 'user21@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586537, 4, 1),
+(115, '竭诚培训公司22', NULL, 'user22@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586537, 4, 1),
+(116, '竭诚培训公司23', NULL, 'user23@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586537, 4, 1),
+(117, '竭诚培训公司24', NULL, 'user24@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586537, 4, 1),
+(118, '竭诚培训公司25', NULL, 'user25@jiecheng.com', '25f9e794323b453885f5181f1b624d0b', 1412586537, 4, 1);
+
+
+
