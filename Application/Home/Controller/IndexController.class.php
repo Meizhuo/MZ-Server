7337582624f02b47e5cb3315c4aa9d173bf70630
Jayin Ton <tonjayin@gmail.com>
2014-10-18 16:18:09 +0800

    update sql

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -7,25 +7,27 @@ class IndexController extends BaseController {
     public function index() {
         $this->redirect('admin/institution/index');
     }
-    
-    public function upload(){
+
+    public function upload() {
         $this->display();
     }
-    
-    public function test(){
+
+    public function test() {
         $this->display();
     }
-    
-    public function mytest(){
-//         print_r(json_encode(D('Document')->getDocumentInfo(166)));
-//         print_r(D('DocumentFile')->linkToDoc(2,array(67,68)));
-//             print_r(D('DocumentCategory')->getCategoryById(1));
-        
+
+    public function mytest() {
+        // print_r(json_encode(D('Document')->getDocumentInfo(166)));
+        // print_r(D('DocumentFile')->linkToDoc(2,array(67,68)));
+        // print_r(D('DocumentCategory')->getCategoryById(1));
         print_r(D('Document')->getDocumentInfo(166));
     }
-    
-    public function change(){
+
+    public function change() {
         $user_ins = D('UserInstitution')->select();
-        print_r($user_ins);
+        foreach ($user_ins as $ins) {
+            $ins['validity_date'] = date('Y-m-d', $ins['validity_date']);
+            D('UserInstitution')->save($ins);
+        }
     }
 }
