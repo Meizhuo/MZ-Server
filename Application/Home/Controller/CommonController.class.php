d57b384c8a66659b8276137a2a568f573ca2a96f
Jayin Ton <tonjayin@gmail.com>
2014-11-20 16:44:48 +0800

    * 下载页修改 * 加入二维码接口 rel #125

---
+++ Application/Home/Controller/CommonController.class.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Home\Controller;
+
+use Common\Controller\BaseController;
+/*
+ * 常用接口
+ * @author Jayin Ton
+ *
+ */
+class CommonController extends BaseController {
+	/**
+	 * 生成二维码
+	 * @param string $text 二维码内容
+	 * @param number $size 二维码长宽,一般为25的倍数,单位：px
+	 * @param number $margin 外边距 单位:px
+	 */
+	public function createQRcode($text = '',$size = 50, $margin = 1){
+		vendor('QRcode.phpqrcode');
+		\QRcode::png($text,false,QR_ECLEVEL_L,$size/25,$margin);
+	}
+}
+
+
--- Docs/Api/api_Common.md
+++ Docs/Api/api_Common.md
@@ -16,3 +16,16 @@ Common
     "response"："清理完毕"
 }
 ```
+
+### 生成二维码
+`GET /home/common/createQRcode` 
+
+字段  |描述 |  是否必须 
+------------ | -------------| -------------
+text | 二维码内容     | Y
+size  | 图像长宽 二维码长宽,一般为25的倍数,单位：px | N
+margin | 外边距 单位:px | N
+ 
+**Response**
+
+二进制图片
