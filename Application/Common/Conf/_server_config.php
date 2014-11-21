b1b05bbe2b8444ac806f8249d618920cbad1ae82
Jayin Ton <tonjayin@gmail.com>
2014-11-21 09:30:32 +0800

    enhance the config

--- Application/Common/Conf/_server_config.php
+++ Application/Common/Conf/_server_config.php
@@ -1,29 +1,7 @@
 <?php
 //上线版本的配置参考
 return array(
-        'DB_TYPE' => 'mysql', // 数据库类型
-        'DB_HOST' => 'localhost', // 服务器地址
-        'DB_NAME' => 'mz', // 数据库名
         'DB_USER' => 'root', // 用户名
         'DB_PWD' => 'root', // 密码
         'DB_PORT' => '3306', // 端口
-        'DB_PREFIX' => 'mz_', // 数据库表前缀
-        // 'SHOW_PAGE_TRACE' => true, // 显示页面Trace信息
-        'LOAD_EXT_FILE' => 'functions', // 自定义加载函数文件
-         /////自定义参数
-        'UPLOAD_PATH' => './Uploads/', // 上传路径
-        'UPLOAD_PATH_ABS' => '/mz/Uploads/', // 上传相对项目路径
-//         'APP_URL' => 'http://61.29.161.61:88/mz/index.php', //product
-//         'APP_URL' => 'http://localhost/mz/index.php',//test
-        //邮件配置
-        'THINK_EMAIL' => array(
-                'SMTP_HOST'   => 'smtp.yeah.net', //SMTP服务器
-                'SMTP_PORT'   => '465', //SMTP服务器端口
-                'SMTP_USER'   => 'dgjnpx@yeah.net', //SMTP服务器用户名
-                'SMTP_PASS'   => 'dgjnpx2014', //SMTP服务器密码
-                'FROM_EMAIL'  => 'dgjnpx@yeah.net', //发件人EMAIL
-                'FROM_NAME'   => '东莞技能培训', //发件人名称
-                'REPLY_EMAIL' => '', //回复EMAIL（留空则为发件人EMAIL）
-                'REPLY_NAME'  => '', //回复名称（留空则为发件人名称）
-        ),
 );
--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,34 +1,43 @@
 <?php
+//线上线下公共配置
+$common_config = array(
+        'URL_MODEL' => 2, //URL模式 REWRITE模式
+        'DB_TYPE' => 'mysql', // 数据库类型
+        'DB_HOST' => 'localhost', // 服务器地址
+        'DB_NAME' => 'mz', // 数据库名
+        // 用户名&密码&端口 在server_config.php中配置
+        // 'DB_USER' => 'root', // 用户名
+        // 'DB_PWD' => 'root', // 密码
+        // 'DB_PORT' => '3306', // 端口
+        'DB_PREFIX' => 'mz_', // 数据库表前缀
+        'LOAD_EXT_FILE' => 'functions', // 自定义加载函数文件
+         /////自定义参数
+        'UPLOAD_PATH' => './Uploads/', // 上传路径
+        'UPLOAD_PATH_ABS' => '/mz/Uploads/', // 上传相对项目路径
+        //邮件配置
+        'THINK_EMAIL' => array(
+                'SMTP_HOST'   => 'smtp.yeah.net', //SMTP服务器
+                'SMTP_PORT'   => '465', //SMTP服务器端口
+                'SMTP_USER'   => 'dgjnpx@yeah.net', //SMTP服务器用户名
+                'SMTP_PASS'   => 'dgjnpx2014', //SMTP服务器密码
+                'FROM_EMAIL'  => 'dgjnpx@yeah.net', //发件人EMAIL
+                'FROM_NAME'   => '东莞技能培训', //发件人名称
+                'REPLY_EMAIL' => '', //回复EMAIL（留空则为发件人EMAIL）
+                'REPLY_NAME'  => '', //回复名称（留空则为发件人名称）
+        ),
+);
+
+//线上配置，只要有server_confi.php这个文件 就会读取这里的配置
 $_Server_Config_File = CONF_PATH.'/server_config.php';
 if(file_exists($_Server_Config_File)){
-    return require($_Server_Config_File);
+    $config = require($_Server_Config_File);
+    return array($config,$common_config);
 }else{
     //默认的测试环境配置
-    return array(
-            'DB_TYPE' => 'mysql', // 数据库类型
-            'DB_HOST' => 'localhost', // 服务器地址
-            'DB_NAME' => 'mz', // 数据库名
+    return array($common_config,array(
+            'SHOW_PAGE_TRACE' => true, // 显示页面Trace信息
             'DB_USER' => 'root', // 用户名
             'DB_PWD' => 'root', // 密码
             'DB_PORT' => '3306', // 端口
-            'DB_PREFIX' => 'mz_', // 数据库表前缀
-            'SHOW_PAGE_TRACE' => true, // 显示页面Trace信息
-            'LOAD_EXT_FILE' => 'functions', // 自定义加载函数文件
-             /////自定义参数
-            'UPLOAD_PATH' => './Uploads/', // 上传路径
-            'UPLOAD_PATH_ABS' => '/mz/Uploads/', // 上传相对项目路径
-    //         'APP_URL' => 'http://61.29.161.61:88/mz/index.php', //product
-    //         'APP_URL' => 'http://localhost/mz/index.php',//test
-            //邮件配置
-            'THINK_EMAIL' => array(
-                    'SMTP_HOST'   => 'smtp.yeah.net', //SMTP服务器
-                    'SMTP_PORT'   => '465', //SMTP服务器端口
-                    'SMTP_USER'   => 'dgjnpx@yeah.net', //SMTP服务器用户名
-                    'SMTP_PASS'   => 'dgjnpx2014', //SMTP服务器密码
-                    'FROM_EMAIL'  => 'dgjnpx@yeah.net', //发件人EMAIL
-                    'FROM_NAME'   => '东莞技能培训', //发件人名称
-                    'REPLY_EMAIL' => '', //回复EMAIL（留空则为发件人EMAIL）
-                    'REPLY_NAME'  => '', //回复名称（留空则为发件人名称）
-            ),
-    );
+    ));
 }
